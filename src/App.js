3441351d058d8fc97645993f5d80a99fd9a4a0fb
jmach83 <jmach83@gmail.com>
2018-11-30 23:33:26 +0100

    add movie table

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
+import Movies from "./components/movies";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="container">
-        <h1>Hello World</h1>
-      </div>
+      <main className="container">
+        <Movies />
+      </main>
     );
   }
 }
