5ebcebd681be63b0bdb10290248e03f7d6050b5d
ASOL\jma <jma@alpha-solutions.dk>
2019-01-14 14:14:57 +0100

    Adding React Router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
+import { Route } from 'react-router-dom';
 import Movies from "./components/movies";
+import Customers from './components/customers';
+import Rentals from './components/rentals';
+import NotFound from './components/notFound';
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <main className="container">
-        <Movies />
+        {/* <Movies />*/}
+        <Route path="/movies" component={Movies}></Route>
+        <Route path="/customers" component={Customers}></Route>
+        <Route path="/rentals" component={Rentals}></Route>
+        <Route path="/not-found" component={NotFound}></Route>
       </main>
     );
   }
