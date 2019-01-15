687dfc2993dc3d2cfe952881499f350c1f429633
jmach83 <jmach83@gmail.com>
2019-01-15 18:23:29 +0100

    Extracting Helper Rendering Methods

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,56 +1,39 @@
-import React, { Component } from 'react';
-import Joi from 'joi-browser';
-import Form from './common/form';
-import Input from './common/input';
+import React from "react";
+import Joi from "joi-browser";
+import Form from "./common/form";
 
 class LoginForm extends Form {
-    state = {
-        data: { username: '', password: ''},
-        errors: {}
-    }
+  state = {
+    data: { username: "", password: "" },
+    errors: {}
+  };
 
-    schema = {
-        username: Joi.string().required().label('Username'),
-        password: Joi.string().required().label('Password')
-    }
+  schema = {
+    username: Joi.string()
+      .required()
+      .label("Username"),
+    password: Joi.string()
+      .required()
+      .label("Password")
+  };
 
-    doSubmit = () =>{
-        // call the server
-        console.log('Submitted');
-    }
+  doSubmit = () => {
+    // call the server
+    console.log("Submitted");
+  };
 
-    render() { 
-        const { data, errors } = this.state;
-
-        return ( 
-            <div>
-                <h1>Login</h1>
-                <form onSubmit={this.handleSubmit}>
-                    <Input 
-                        name="username" 
-                        value={data.username} 
-                        label="Username" 
-                        onChange={this.handleChange}
-                        error={errors.username} 
-                    />
-                    <Input 
-                        name="password" 
-                        value={data.password} 
-                        label="Password" 
-                        onChange={this.handleChange}
-                        error={errors.password} 
-
-                    />                    
-                    <button 
-                        disabled={this.validate()} 
-                        className="btn btn-primary"
-                    >
-                        Login
-                    </button>
-                </form>
-            </div> 
-        );
-    }
+  render() {
+    return (
+      <div>
+        <h1>Login</h1>
+        <form onSubmit={this.handleSubmit}>
+          {this.renderInput("username", "Username")}
+          {this.renderInput("password", "Password", "password")}
+          {this.renderButton("Login")}
+        </form>
+      </div>
+    );
+  }
 }
- 
+
 export default LoginForm;
