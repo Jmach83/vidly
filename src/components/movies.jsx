6c2a31b17c3c2f53b450c39da486255ce1446913
ASOL\jma <jma@alpha-solutions.dk>
2019-01-16 17:36:48 +0100

    Replaceing FakeMovieService

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Link } from 'react-router-dom';
 import { getMovies } from "../services/fakeMovieService";
-import { getGenres } from "../services/fakeGenreService";
+import { getGenres } from "../services/genreService";
 import Pagination from "./common/pagination";
 import ListGroup from "./common/listGroup";
 import { paginate } from "../utils/paginate";
@@ -21,8 +21,9 @@ class Movies extends Component {
     sortColumn: { path: "title", order: "asc" }
   };
 
-  componentDidMount() {
-    const genres = [{ _id: "", name: "All Genres" }, ...getGenres()];
+  async componentDidMount() {
+    const { data } = await getGenres();
+    const genres = [{ _id: "", name: "All Genres" }, ...data];
     this.setState({ movies: getMovies(), genres });
   }
 
--- src/services/logService.js
+++ src/services/logService.js
@@ -5,7 +5,7 @@ function init(){
     //     dsn: "https://324ffedf829a4ec4b6d263d5f4fe7b55@sentry.io/1372975",
     //     release: '1-0-0',
     //     environment: 'development-test'
-    });
+    // });
 }
 
 function log(error){
